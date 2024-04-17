2100318bd0f47006c9e88c73bb26e0bdf7cad4cb
chandraSekhar-alti <althichandu123@gmail.com>
2024-04-17 11:17:13 +0530

    implemented .env file configuration

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -9,22 +9,26 @@ import org.testng.ITestResult;
 import org.testng.annotations.*;
 import Utils.*;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
 public  class BaseTest {
 
 
     public static WebDriver driver;
-
+    protected static Properties properties;
 
     @BeforeMethod
     public void setup(ITestResult result) {
+        loadProperties();
 
         // Set up WebDriver and open the browser
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-
-        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-
+        String BASE_URL = properties.getProperty("baseUrl");
+        driver.get(BASE_URL);
+        //wait until to load complete page
         UI.waitForElement(By.className("orangehrm-login-logo"));
         loginUser();
         UI.waitForElement(By.xpath("//img[@alt='client brand banner']"));
@@ -32,7 +36,6 @@ public  class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-
         if (driver != null) {
             driver.quit();
         }
@@ -40,10 +43,24 @@ public  class BaseTest {
 
 
     public static void loginUser() {
+
+        String username = properties.getProperty("username");
+        String password = properties.getProperty("password");
+
         // Perform login steps
-        UI.sendText(By.xpath("//input[@name='username']"), "Admin");
-        UI.sendText(By.xpath("//input[@type='password']"), "admin123");
+        UI.sendText(By.xpath("//input[@name='username']"), username);
+        UI.sendText(By.xpath("//input[@type='password']"), password);
         UI.clickElement(By.xpath("//button[@type='submit']"));
+    }
+
+    // Load properties from config.properties file
+    private void loadProperties() {
+        properties = new Properties();
+        try (FileInputStream inputStream = new FileInputStream("src/test/resources/config.properties")) {
+            properties.load(inputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
