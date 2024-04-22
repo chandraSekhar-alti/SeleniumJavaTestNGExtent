ef70bacf6ff45779f850f00578bb810adb41abb1
chandraSekhar-alti <althichandu123@gmail.com>
2024-04-22 18:54:08 +0530

    integrated static reports

--- src/test/java/Accessibility/AccessibilityBaseTest.java
+++ src/test/java/Accessibility/AccessibilityBaseTest.java
@@ -3,21 +3,23 @@ package Accessibility;
 import Utils.BrowserActions;
 import Utils.UI;
 import com.deque.html.axecore.results.Results;
+import com.deque.html.axecore.results.Rule;
 import com.deque.html.axecore.selenium.AxeBuilder;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.util.Collections;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
-
-//import static Tests.BaseTest.driver;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class AccessibilityBaseTest {
     protected WebDriver driver;
+
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
@@ -35,7 +37,7 @@ public class AccessibilityBaseTest {
 
     protected Results performAccessibilityAnalysis() {
         AxeBuilder axeBuilder = new AxeBuilder();
-        return axeBuilder.analyze(driver); // Perform analysis
+        return axeBuilder.analyze(driver);
     }
 
     protected Results performAccessibilityAnalysisWithExclusions(List<String> rulesToExclude) {
@@ -45,5 +47,26 @@ public class AccessibilityBaseTest {
 
         AxeBuilder axeBuilder = new AxeBuilder().disableRules(rulesToExclude);
         return axeBuilder.analyze(driver);
+    }
+
+    protected void saveAccessibilityResultsToJson(Results results, String filePath) {
+        JSONArray violationsArray = new JSONArray();
+
+        List<Rule> violations = results.getViolations(); // Get violations from Results
+        for (Rule violation : violations) {
+            JSONObject violationJson = new JSONObject();
+            violationJson.put("id", violation.getId());
+            violationJson.put("impact", violation.getImpact());
+            violationJson.put("description", violation.getDescription());
+            violationJson.put("helpUrl", violation.getHelpUrl());
+
+            violationsArray.put(violationJson); // Add each violation to the JSON array
+        }
+
+        try (FileWriter fileWriter = new FileWriter(new File(filePath))) {
+            fileWriter.write(violationsArray.toString(4)); // Save formatted JSON to the file
+        } catch (IOException e) {
+            System.err.println("Error saving accessibility results: " + e.getMessage());
+        }
     }
 }
