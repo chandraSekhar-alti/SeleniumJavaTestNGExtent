4d9026f936826084d7590c3e1f579d6658536ad5
chandraSekhar-alti <althichandu123@gmail.com>
2024-05-14 14:04:05 +0530

    added JWT token automation script

--- src/test/java/API/Tests/AuthOperations/JWTTokenTest.java
+++ src/test/java/API/Tests/AuthOperations/JWTTokenTest.java
@@ -1,2 +1,32 @@
-package API.Tests.AuthOperations;public class JWTTokenTest {
+package API.Tests.AuthOperations;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class JWTTokenTest {
+
+    @Test
+    public static void testGetJWTToken(){
+        Response response = RestAssured.given()
+                .contentType("application/json")
+                .body("{\"username\":\"chandraalti8978@gmail.com\", \"password\":\"Amma123@\"}")
+                .post("https://www.99.co/");
+
+        // Log the response body to see what was returned
+        System.out.println("Response status code: " + response.getStatusCode());
+        System.out.println("Response body: " + response.getBody().asPrettyString());
+        // Response logging to understand what's happening
+        System.out.println("Status code: " + response.getStatusCode());
+        System.out.println("Headers: " + response.getHeaders());
+        System.out.println("Response body: " + response.getBody().asString());
+
+
+        try {
+            String token = response.jsonPath().getString("token");
+            System.out.println("Token is: " + token);
+        } catch (Exception e) {
+            System.out.println("Error extracting token: " + e.getMessage());
+        }
+    }
 }
