415da6dbde7fd435a0e47c3c6c2389aec4b5ace9
chandraSekhar-alti <althichandu123@gmail.com>
2024-05-16 15:31:22 +0530

    fixed extent reports and added failure screenshot functionality

--- src/test/java/Accessibility/AccessibilityBaseTest.java
+++ src/test/java/Accessibility/AccessibilityBaseTest.java
@@ -3,15 +3,23 @@ package Accessibility;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.devtools.v85.layertree.model.StickyPositionConstraint;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import java.lang.reflect.Method;
@@ -39,7 +47,18 @@ public class AccessibilityBaseTest {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            String screenshotPath = captureScreenshot(driver, result.getName());
+            test.fail("Test Failed: " + result.getThrowable(), MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
+        } else if (result.getStatus() == ITestResult.SKIP) {
+            test.skip("Test Skipped: " + result.getThrowable());
+        } else {
+            test.pass("Test Passed");
+        }
+
+        extentReports.flush();
+
         if (driver != null) {
             driver.quit();
             driver = null;
@@ -56,6 +75,21 @@ public class AccessibilityBaseTest {
         ExtentSparkReporter htmlReporter = new ExtentSparkReporter(System.getProperty("user.dir") + "/src/test/java/Accessibility/Reports/accessibility_report.html");
         extentReports = new ExtentReports();
         extentReports.attachReporter(htmlReporter);
+    }
+
+    private String captureScreenshot(WebDriver driver, String screenshotName){
+        try {
+            TakesScreenshot takesScreenshot = (TakesScreenshot) driver;
+            File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+            String screenshotPath = System.getProperty("user.dir") + "src/test/java/Accessibility/Reports/Screenshots/" + screenshotName +"_screenshot.png";
+            File destination  = new File(screenshotPath);
+            FileUtils.copyFile(source, destination );
+            return screenshotPath;
+
+        } catch (IOException e) {
+            System.out.println("Expection while taking screenshot :: "+e.getMessage());
+            throw new RuntimeException(e);
+        }
     }
 
 
